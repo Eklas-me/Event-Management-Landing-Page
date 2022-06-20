9b9e61c8b139c5db78fb0f91335411ca6dc1dc46
Eklas-me <kh.Eklas502@gmail.com>
2022-06-20 23:10:38 +0600

    last

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Event-Management-Landing-Page
+echo "# Event-Management-Landing-Page" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Eklas-me/Event-Management-Landing-Page.git
+git push -u origin main
--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
+// JQuery Counter
 $('.counter').counterUp({
   delay: 10,
   time: 1000
 });
 
 // prining switch
-
 const pricing_switch = document.querySelector(".switch-button-checkbox");
 const yearly = document.querySelector(".yearly");
 const monthly =document.querySelector(".monthly");
@@ -20,3 +20,23 @@ pricing_switch.addEventListener("change", function(){
     yearly.classList.add("active");
   }
 } , false);
+
+
+// Owl Carousel
+//default settings:
+
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:4,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:true
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})
